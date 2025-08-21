2fdf5a08bb2caaa6d3b3338eb50be63d5e2cdb13
jimbomonkey <annex12@hotmail.com>
2025-08-21 11:12:44 +0100

    Create constants.py and add APP_NAME

--- gui/main_window.py
+++ gui/main_window.py
@@ -1,6 +1,8 @@
 from PyQt6 import QtWidgets
 from typing import Optional
 
+from constants import APP_NAME
+
 
 class MainWindow(QtWidgets.QMainWindow):
     def __init__(self, parent: Optional[QtWidgets.QWidget] = None) -> None:
@@ -8,4 +10,4 @@ class MainWindow(QtWidgets.QMainWindow):
         self._init_ui()
 
     def _init_ui(self) -> None:
-        self.setWindowTitle("DiscogsRenamer")
+        self.setWindowTitle(APP_NAME)

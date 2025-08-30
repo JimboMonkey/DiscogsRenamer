77eabe3defc6cac6f5209d3dc9f9cff7677912fc
jimbomonkey <annex12@hotmail.com>
2025-08-30 22:41:18 +0100

    Add label to token dialog, rename button label

--- gui/token_dialog.py
+++ gui/token_dialog.py
@@ -14,6 +14,7 @@ class TokenDialogGui(QtWidgets.QDialog):
         regex = QtCore.QRegularExpression("[A-Za-z0-9]+")
         validator = QtGui.QRegularExpressionValidator(regex, self)
 
+        self._token_label = QtWidgets.QLabel("Token")
         self._token_lineedit = QtWidgets.QLineEdit(self)
         self._token_lineedit.setPlaceholderText("Enter your Discogs API token")
         self._token_lineedit.setMinimumWidth(350)
@@ -22,16 +23,17 @@ class TokenDialogGui(QtWidgets.QDialog):
         self._token_lineedit.textChanged.connect(self._save_button_enabled)
 
         self._save_button = QtWidgets.QPushButton("Save")
-        self._cancel_button = QtWidgets.QPushButton("Cancel")
+        self._close_button = QtWidgets.QPushButton("Close")
 
         self._save_button.setEnabled(False)
         # self._save_button.clicked.connect(self._save_token)
-        self._cancel_button.clicked.connect(self._close_dialog)
+        self._close_button.clicked.connect(self._close_dialog)
 
         self._horizontal_layout = QtWidgets.QHBoxLayout(self)
+        self._horizontal_layout.addWidget(self._token_label)
         self._horizontal_layout.addWidget(self._token_lineedit)
         self._horizontal_layout.addWidget(self._save_button)
-        self._horizontal_layout.addWidget(self._cancel_button)
+        self._horizontal_layout.addWidget(self._close_button)
 
         self.setLayout(self._horizontal_layout)
 

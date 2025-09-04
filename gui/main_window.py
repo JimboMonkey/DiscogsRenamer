1662aa9deb70003f07baa89e7596711fac63acd9
jimbomonkey <annex12@hotmail.com>
2025-09-04 12:30:57 +0100

    Basic main window layout

--- gui/main_window.py
+++ gui/main_window.py
@@ -13,7 +13,47 @@ class MainWindow(QtWidgets.QMainWindow):
 
     def _init_ui(self) -> None:
         self.setWindowTitle(APP_NAME)
-        self.open_token_dialog()
+
+        release_label = QtWidgets.QLabel("Release")
+        folder_label = QtWidgets.QLabel("Folder")
+
+        web_browser_button = QtWidgets.QPushButton("Browse")
+        release_text_entry = QtWidgets.QLineEdit()
+
+        file_browser_button = QtWidgets.QPushButton("Browse")
+        folder_entry_label = QtWidgets.QLabel()
+
+        release_entry_layout = QtWidgets.QHBoxLayout()
+        release_entry_layout.addWidget(web_browser_button)
+        release_entry_layout.addWidget(release_text_entry)
+
+        folder_entry_layout = QtWidgets.QHBoxLayout()
+        folder_entry_layout.addWidget(file_browser_button)
+        folder_entry_layout.addWidget(folder_entry_label)
+
+        release_listwidget = QtWidgets.QListWidget()
+        folder_listwidget = QtWidgets.QListWidget()
+
+        release_layout = QtWidgets.QVBoxLayout()
+        release_layout.addWidget(release_label)
+        release_layout.addLayout(release_entry_layout)
+        release_layout.addWidget(release_listwidget)
+
+        folder_layout = QtWidgets.QVBoxLayout()
+        folder_layout.addWidget(folder_label)
+        folder_layout.addLayout(folder_entry_layout)
+        folder_layout.addWidget(folder_listwidget)
+
+        listwidget_layout = QtWidgets.QHBoxLayout()
+        listwidget_layout.addLayout(release_layout)
+        listwidget_layout.addLayout(folder_layout)
+
+        # Set the listwidget layout as the layout for the window
+        central_widget = QtWidgets.QWidget(self)
+        central_widget.setLayout(listwidget_layout)
+        self.setCentralWidget(central_widget)
+
+        # self.open_token_dialog()
 
     def open_token_dialog(self) -> None:
         dialog = TokenDialogGui()

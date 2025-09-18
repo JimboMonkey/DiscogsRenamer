fe08150e8702399b699857e0cecf2701f3ced9f4
jimbomonkey <annex12@hotmail.com>
2025-09-18 13:41:45 +0100

    Tidy up main_manager functions and variable names

--- main_manager.py
+++ main_manager.py
@@ -4,6 +4,7 @@ from gui.main_window import MainWindow
 
 from pathlib import Path
 from os.path import expanduser
+from typing import List
 
 
 class MainManager(QtCore.QObject):
@@ -11,11 +12,12 @@ class MainManager(QtCore.QObject):
     def __init__(self) -> None:
         # Create the main GUI window
         self._ui = MainWindow()
-
-        self._ui.file_browser_button.pressed.connect(self._show_open_dialog)
         super(MainManager, self).__init__()
 
-    # Return a QFileDialog for 'Open'
+        # Connect signals and slots
+        self._ui.file_browser_button.pressed.connect(self._show_open_dialog)
+
+    # Open Folder dialog and return the selected path
     def _open_dialog(self) -> str:
         return QtWidgets.QFileDialog.getExistingDirectory(
             self._ui,
@@ -27,23 +29,37 @@ class MainManager(QtCore.QObject):
 
     # Use the standard open dialog box to get the file path of a folder
     def _show_open_dialog(self) -> None:
-        file_path = self._open_dialog()
+        folder_path = self._open_dialog()
 
         # Check for file validity here so that cancelling
         # the open dialog doesn't set a blank path
-        if (file_path) and (Path(file_path).exists()):
-            self._open_file(Path(file_path))
+        if (folder_path) and (Path(folder_path).exists()):
+            self._read_folder_contents(Path(folder_path))
 
-    # Set a file path in the GUI, config file, and database
-    # Then trigger a load of its categories
-    def _open_file(self, file_path: Path) -> None:
-        self._ui.folder_entry_label.setText(str(file_path))
-        self._list_files_in_folder(str(file_path))
+    # Set the folder path in the GUI and populate
+    # the folder list with the names of its files
+    def _read_folder_contents(self, folder_path: Path) -> None:
+        self._ui.folder_entry_label.setText(str(folder_path))
+        file_list = self._list_audio_files_in_folder(folder_path)
+        self._ui.populate_folder_list(file_list)
 
-    # List files in a folder matching a pattern
-    def _list_files_in_folder(self, folder_path: str, pattern: str = "*.mp3") -> None:
-        folder = Path(folder_path).expanduser().resolve()
-        if not folder.is_dir():
-            raise ValueError(f"Invalid folder: {folder}")
-        print([str(p) for p in folder.glob(pattern) if p.is_file()])
-        # return [str(p) for p in folder.glob(pattern) if p.is_file()]
+    # Return a sorted list of audio files in a folder
+    def _list_audio_files_in_folder(self, folder_path: Path) -> List[Path]:
+
+        audio_file_extensions = [
+            "*.mp3",
+            "*.wav",
+            "*.flac",
+            "*.m4a",
+            "*.ogg",
+            "*.aiff",
+            "*.alac",
+            "*.wma",
+            "*.aac",
+        ]
+        return [
+            audio_file
+            for extension in audio_file_extensions
+            for audio_file in sorted(folder_path.glob(extension))
+            if audio_file.is_file()
+        ]

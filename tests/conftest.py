8c740eb8364fc5d2087f15c4a0b49e075416954e
jimbomonkey <annex12@hotmail.com>
2025-11-15 21:18:09 +0000

    Add artist and title to mock_discogs_release

--- tests/conftest.py
+++ tests/conftest.py
@@ -12,6 +12,8 @@ def mock_discogs_client() -> MagicMock:
 @pytest.fixture
 def mock_discogs_release() -> MagicMock:
     mock_release = MagicMock(spec_set=Release)
+    mock_release.artists_sort = "DJ Mock"
+    mock_release.title = "Mock & Test"
     mock_release.tracklist = [
         MagicMock(spec_set=Track, title="Track 1"),
         MagicMock(spec_set=Track, title="Track 2"),

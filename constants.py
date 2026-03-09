ecb17c9df7c56e85513091a82eb6d56327a73c00
jimbomonkey <annex12@hotmail.com>
2026-03-09 22:57:16 +0000

    Read version number from pyproject.toml

--- constants.py
+++ constants.py
@@ -1,2 +1,12 @@
+import tomllib
+from importlib.metadata import version, PackageNotFoundError
+from pathlib import Path
+
 APP_NAME = "DiscogsRenamer"
-APP_VERSION = "1.0.0"
+PACKAGE_NAME = "discogsrenamer"
+
+try:
+    APP_VERSION = version(PACKAGE_NAME)
+except PackageNotFoundError:
+    data = tomllib.loads(Path("pyproject.toml").read_text())
+    APP_VERSION = data["project"]["version"]

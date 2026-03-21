7184cf8fd808fb3b86539e6190d896c03819217b
jimbomonkey <annex12@hotmail.com>
2026-03-21 23:32:25 +0000

    Add comment to setup-system.sh

--- scripts/setup-system.sh
+++ scripts/setup-system.sh
@@ -1,6 +1,9 @@
 sudo apt update
+
+# Python environment tools
 sudo apt install pipx
 pipx ensurepath
 pipx install uv
+
 # The following are required for running PyQt6 apps
 sudo apt install libxcb-cursor0 libx11-dev libxext-dev

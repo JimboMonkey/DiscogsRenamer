974c55a99721cca7fa1745211205110c512b8fb3
jimbomonkey <annex12@hotmail.com>
2026-03-24 22:33:22 +0000

    Rework the way the app version is fetched
    Removes relying on knowing the path to pyproject.toml

--- discogsrenamer/core/constants.py
+++ discogsrenamer/core/constants.py
@@ -1,12 +1,4 @@
-import tomllib
-from importlib.metadata import version, PackageNotFoundError
-from pathlib import Path
+from version import VERSION
 
 APP_NAME = "DiscogsRenamer"
-PACKAGE_NAME = "discogsrenamer"
-
-try:
-    APP_VERSION = version(PACKAGE_NAME)
-except PackageNotFoundError:
-    data = tomllib.loads(Path("pyproject.toml").read_text())
-    APP_VERSION = data["project"]["version"]
+APP_VERSION = VERSION
